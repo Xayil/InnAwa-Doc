c18a7323084045fd02b0735747c06d1488192069
xayil <xayil.gph@gmail.com>
2023-06-13 14:58:25 -0600

    Create Doc_RouteApp.swift

---
+++ Doc_RouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doc_RouteApp.swift
+//  Doc Route
+//
+//  Created by Oziel Alameda on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Doc_RouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
